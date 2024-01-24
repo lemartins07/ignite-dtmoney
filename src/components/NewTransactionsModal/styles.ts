4ace8fe4c7abdcfa7810202ff283068fc6fe34f9
Leandro Martins <leandro.marttins.85@gmail.com>
2024-01-24 17:36:21 -0300

    Aplicando React Hook Form

--- src/components/NewTransactionsModal/styles.ts
+++ src/components/NewTransactionsModal/styles.ts
@@ -49,7 +49,13 @@ export const Content = styled(Dialog.Content)`
       border-radius: 6px;
       margin-top: 1.25rem;
       cursor: pointer;
-      &:hover {
+
+      &:disabled {
+        opacity: 0.5;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background: ${(props) => props.theme['green-700']};
         transition: background-color 0.2s;
       }
